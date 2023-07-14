36aa95f8d1a2af305a5382dcf73d1874208bbe5d
jingll <the2ndindec@hotmail.com>
2023-07-14 11:20:15 +0800

    feat: init repo

---
+++ backend/src/main/java/cn/master/backend/BackendApplication.java
@@ -0,0 +1,16 @@
+package cn.master.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 11's papa
+ */
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
